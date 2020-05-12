315bd34fcb1cb6a3b36918e1682a84117148d2e9
will889889 <45516406+will889889@users.noreply.github.com>
2020-05-12 23:10:51 +0800

    add invert

--- Robot-RX78/Robot/gray.fs.glsl
+++ Robot-RX78/Robot/gray.fs.glsl
@@ -1,6 +1,7 @@
 #version 410 core                                                              
 
-uniform sampler2D tex;                                                         
+uniform sampler2D tex;
+uniform float colorError = 0.0f;
 
 out vec4 color;                                                                
 
@@ -9,9 +10,15 @@ in VS_OUT
 vec2 texcoord;                                                             
 } fs_in;                                                                       
 
+float PI = 3.14159;
+
 void main(void)                                                                
 {                                                                              
-	vec4 texture_color = texture(tex, fs_in.texcoord);							
-	float grayscale_color = 0.299*texture_color.r + 0.587*texture_color.g + 0.114*texture_color.b;
-	color = vec4(grayscale_color, grayscale_color, grayscale_color, 1.0);
+	vec4 texture_color = texture(tex, fs_in.texcoord);
+	color = vec4(
+		(1 - texture_color.r * sin(2 * PI * colorError)), 
+		(1 - texture_color.g * sin(2 * PI * colorError)), 
+		(1 - texture_color.b * sin(2 * PI * colorError)), 
+		1.0);
+	
 }                                                                              
